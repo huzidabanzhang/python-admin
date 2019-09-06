278ce7be8e59bac8485f116c638aa4deb1063f9a
大胡子 <375532103@qq.com>
2019-09-06 11:16:52 +0800

    权限表设计（进行中）

--- trunk/models/user.py
+++ trunk/models/user.py
@@ -5,15 +5,19 @@ import datetime
 
 
 class User(db.Model):
+    '''
+    用户
+    '''
     __tablename__ = 'db_user'
     id = db.Column(db.Integer, nullable=False, primary_key=True, index=True, autoincrement=True)
     user_id = db.Column(db.String(36), index=True, nullable=False, unique=True)
     username = db.Column(db.String(64), index=True, nullable=False, unique=True)
     password = db.Column(db.String(32), nullable=False)
     nickname = db.Column(db.String(64))
-    sex = db.Column(db.INT(), default=1)
-    create_time = db.Column(db.DateTime(), default=datetime.datetime.now)
-    update_time = db.Column(db.DateTime(), default=datetime.datetime.now)
+    sex = db.Column(db.SmallInteger, default=1)
+    role_id = db.Column(db.Integer, db.ForeignKey('db_role.id'))
+    create_time = db.Column(db.DateTime, index=True, default=datetime.datetime.now)
+    update_time = db.Column(db.DateTime, default=datetime.datetime.now)
     __table_args__ = ({"useexisting": True})
 
     def is_authenticated(self):
@@ -30,3 +34,48 @@ class User(db.Model):
 
     def __repr__(self):
         return '<User %r>' % self.username
+
+
+class Role(db.Model):
+    '''
+    权限
+    '''
+    __tablename__ = 'db_role'
+    id = db.Column(db.Integer, nullable=False, primary_key=True, index=True, autoincrement=True)
+    name = db.Column(db.String(64), nullable=False, unique=True)
+    type = db.Column(db.SmallInteger, index=True, default=1)
+    users = db.relationship('User', backref='role')
+    menus = db.relationship('Menu',
+                            secondary=RoleToMenu,
+                            backref=db.backref('db_role', lazy='dynamic'),
+                            lazy='dynamic')
+    __table_args__ = ({"useexisting": True})
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
+
+
+RoleToMenu = db.Table(
+    'db_role_to_menu',
+    db.Column('role_id', db.Integer, db.ForeignKey('db_role.id')),
+    db.Column('menu_id', db.Integer, db.ForeignKey('db_menu.id'))
+)
+
+
+def Menu(db.Model):
+    '''
+    菜单
+    '''
+    __tablename__ = 'db_menu'
+    id = db.Column(db.Integer, nullable=False, primary_key=True, index=True, autoincrement=True)
+    parentId = db.Column(db.Integer, nullable=False, index=True, default=0)
+    title = db.Column(db.String(64), nullable=False, unique=True)
+    path = db.Column(db.String(255), nullable=False)
+    icon = db.Column(db.String(255), nullable=False)
+    sort = db.Column(db.SmallInteger, index=True, default=1)
+    type = db.Column(db.SmallInteger, index=True, default=1)
+    isLock = db.Column(db.Boolean, index=True, default=True)
+    __table_args__ = ({"useexisting": True})
+
+    def __repr__(self):
+        return '<Menu %r>' % self.title
