2a2c46826fd28f8ed62dc10ede1f3f96f7deb7c5
大胡子 <375532103@qq.com>
2020-04-21 10:19:15 +0800

    验证器完成 暂时验证int list str boolean phone email ic这些类型的格式 required必填 后续会加新的验证判断

--- trunk/validate/__init__.py
+++ trunk/validate/__init__.py
@@ -5,10 +5,11 @@
 @Author: Zpp
 @Date: 2020-04-20 13:43:42
 @LastEditors: Zpp
-@LastEditTime: 2020-04-20 16:51:11
+@LastEditTime: 2020-04-21 10:13:53
 '''
 from flask import request
 from libs.code import ResultDeal
+import re
 
 class validate_form():
     '''
@@ -16,24 +17,48 @@ class validate_form():
     '''
     def __init__(self, params):
         self.params = params
+        self.phone = re.compile('^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-7|9])|(?:5[0-3|5-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[1|8|9]))\d{8}$')
+        self.id_card = re.compile('^\d{6}(18|19|20)\d{2}(0\d|10|11|12)([0-2]\d|30|31)\d{3}[\dXx]$')
+        self.email = re.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
-    def validate_params(self, f, i):
+    def validate_params(self, f, i, fun):
         '''
         验证字段
         '''
+        error = False
+
         if i['type'] == 'int':
             if type(f) != int:
-                return ResultDeal(code=-1, msg=i['help_msg'])
+                error = True
 
         if i['type'] == 'str':
             if type(f) != str:
-                return ResultDeal(code=-1, msg=i['help_msg'])
+                error = True
 
         if i['type'] == 'boolean':
             if f != 'true' and f != 'false':
-                return ResultDeal(code=-1, msg=i['help_msg'])
+                error = True
 
+        if i['type'] == 'list':
+            if type(f) != list:
+                error = True
+
+        if i['type'] == 'ic':
+            if not self.id_card.match(f):
+                error = True
         
+        if i['type'] == 'phone':
+            if not self.phone.match(f):
+                error = True
+
+        if i['type'] == 'email':
+            if not self.email.match(f):
+                error = True
+                
+        if error == True:
+            return ResultDeal(code=-1, msg=i['help_msg'])
+        else:
+            return fun()
 
     def form(self, args):
         '''
@@ -43,16 +68,18 @@ class validate_form():
             def one():
                 if self.params.has_key(args):
                     for i in self.params[args]:
+                        data = request.form.getlist(i['name']) if i['type'] == 'list' else request.form.get(i['name'])
+
                         if not i.has_key('required') or not i['required']:
-                            if request.form.get(i['name']):
-                                return self.validate_params(request.form.get(i['name']), i)
+                            if data:
+                                return self.validate_params(data, i, f)
                         else:
-                            if not request.form.get(i['name']):
+                            if not data:
                                 return ResultDeal(code=-1, msg=i['required_msg'])
                             else:
-                                return self.validate_params(request.form.get(i['name']), i)
+                                return self.validate_params(data, i, f)
 
-                return f
+                return f()
             return one
         
         return validate
