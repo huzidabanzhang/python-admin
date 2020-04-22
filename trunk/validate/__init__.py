9698dda338c876b2c5626b4a12a7ec30b1618edf
大胡子 <375532103@qq.com>
2020-04-22 10:44:28 +0800

    验证器新增 max min default between的判断 验证器基本完成 后续引用到接口上

--- trunk/validate/__init__.py
+++ trunk/validate/__init__.py
@@ -5,7 +5,7 @@
 @Author: Zpp
 @Date: 2020-04-20 13:43:42
 @LastEditors: Zpp
-@LastEditTime: 2020-04-21 10:13:53
+@LastEditTime: 2020-04-22 10:39:25
 '''
 from flask import request
 from libs.code import ResultDeal
@@ -21,19 +21,55 @@ class validate_form():
         self.id_card = re.compile('^\d{6}(18|19|20)\d{2}(0\d|10|11|12)([0-2]\d|30|31)\d{3}[\dXx]$')
         self.email = re.compile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
 
+    def validate_min_max(self, f, i):
+        '''
+        验证字段最大最小值
+        '''
+        error = None
+
+        if i.has_key('max'):
+            if f > i:
+                error = u'%s超出字符限制' % i['name']
+
+        if i.has_key('min'):
+            if f < i:
+                error = u'%s不能少于%s个字符' % (i['name'], i['min'])
+
+        return error
+
+    def validate_between(self, f, i):
+        '''
+        验证字段是否在固定值之间
+        '''
+        error = None
+
+        if i.has_key('between') and type(i['between']) == list:
+            if not f in i['between']:
+                error = u'%s不在规定范围内' % i['name']
+
+        return error
+
     def validate_params(self, f, i, fun):
         '''
         验证字段
         '''
-        error = False
+        error = None
 
         if i['type'] == 'int':
             if type(f) != int:
                 error = True
 
+            error = self.validate_min_max(f, i)
+            if not error:
+                error = self.validate_between(f, i)
+
         if i['type'] == 'str':
             if type(f) != str:
                 error = True
+
+            error = self.validate_min_max(f, i)
+            if not error:
+                error = self.validate_between(f, i)
 
         if i['type'] == 'boolean':
             if f != 'true' and f != 'false':
@@ -56,9 +92,22 @@ class validate_form():
                 error = True
                 
         if error == True:
-            return ResultDeal(code=-1, msg=i['help_msg'])
-        else:
+            return ResultDeal(code=-1, msg=u'%s格式错误' % i['name'])
+        elif error == None:
+            if i.has_key('default'):
+                self.add_default(f, i['default'])
+
             return fun()
+        else:
+            return ResultDeal(code=-1, msg=error)
+
+    def add_default(self, f, default):
+        '''
+        添加默认值
+        '''
+        r = request.form.copy()
+        r[f] = default
+        request.form = r
 
     def form(self, args):
         '''
@@ -68,14 +117,17 @@ class validate_form():
             def one():
                 if self.params.has_key(args):
                     for i in self.params[args]:
-                        data = request.form.getlist(i['name']) if i['type'] == 'list' else request.form.get(i['name'])
+                        data = request.form.getlist(i['value']) if i['type'] == 'list' else request.form.get(i['value'])
 
                         if not i.has_key('required') or not i['required']:
                             if data:
                                 return self.validate_params(data, i, f)
+                            else:
+                                if i.has_key('default'):
+                                    self.add_default(i['value'], i['default'])
                         else:
                             if not data:
-                                return ResultDeal(code=-1, msg=i['required_msg'])
+                                return ResultDeal(code=-1, msg=u'请输入%s' % i['name'])
                             else:
                                 return self.validate_params(data, i, f)
 
